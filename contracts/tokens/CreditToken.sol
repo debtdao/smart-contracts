a5e91560755b29e691b28dcc30e703625fd38e27
kiba gateaux <kibagateaux@gmail.com>
2022-01-26 20:31:41 +0900

    update CREDIT token to burn set fCREDIT

--- contracts/tokens/CreditToken.sol
+++ contracts/tokens/CreditToken.sol
@@ -7,11 +7,11 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract CreditToken is Ownable, ERC20("Debt DAO Credit v1", "CREDIT-v1") {
 
+    address fCREDIT; // fToken contract address to deposit CREDIT into Rari pool
     uint256 public globalCreditLimit; // maximum allowed CREDIT supply
-    address constant debtFusePool = 0xA097856Ef35D368184DE4c3771E7F363B5Cb01E5; // TODO might need to change/add comptroller/cToken address
 
 
-    mapping(address => bool) minters; // trusted parties to mint CREDIT to borrowers
+    mapping(address => bool) minters; // borrowers to mint CREDIT to borrowers
     mapping(address => bool) userWhitelist; // address able to interact with CREDIT token
 
     event UpdateMinter(address indexed minter, bool indexed allowed);
@@ -20,8 +20,9 @@ contract CreditToken is Ownable, ERC20("Debt DAO Credit v1", "CREDIT-v1") {
 
     constructor(uint256 creditLimit) {
         globalCreditLimit = creditLimit;
-        // Allow CREDIT to be supplied to fuse pool
-        userWhitelist[debtFusePool] = true;
+
+        // allow burn address in whitelist
+        userWhitelist[address(0)] = true;
     }
 
     modifier onlyMinters() {
@@ -39,8 +40,21 @@ contract CreditToken is Ownable, ERC20("Debt DAO Credit v1", "CREDIT-v1") {
     }
 
     /**
-      @dev  Allows approved minters to increase CREDIT supply.
-            Fails if requested mint puts total supply over cap set by DebtDAO governance.
+      @dev Sets the contract address for fCREDIT contract that borrwers will deposit CREDIT into
+      @param fToken - fCREDIT address
+      */
+    function setFuseToken(address fToken) external onlyOwner returns(bool) {
+        require(fCREDIT == address(0), 'CREDIT: fCREDIT already set');
+
+        fCREDIT = fToken;
+        userWhitelist[fCREDIT] = true; // let fCREDIT use CREDIT
+        
+        return true;
+    }
+
+    /**
+      @dev  Allows approved minters to increase circulating CREDIT.
+            Fails if requested mint puts total supply over debt ceiling set by DebtDAO governance.
       */
     function mint(address account, uint256 amount) external onlyMinters returns(bool) {
         require(totalSupply() + amount <= globalCreditLimit, "CREDIT: supply limit reached");
@@ -52,18 +66,29 @@ contract CreditToken is Ownable, ERC20("Debt DAO Credit v1", "CREDIT-v1") {
         }
         _mint(account, amount);
         // automatically approve fuse pool to use tokens because only thing to do with them
-        _approve(account, debtFusePool, amount);
+        _approve(account, fCREDIT, amount);
         
         return true;
     }
 
+    /**
+     *@dev allow borrowers to burn tokens from themselves
+     * @param amount - amount of CREDIT to be burned
+     */
+    function burn(uint256 amount) external returns(bool) {
+        _burn(msg.sender, amount);
+        return true;
+    }
+
+    /**
+     *@dev allow minters to burn tokens from themselves
+     * @param amount - amount of CREDIT to be burned
+     */
 
     function burnFrom(address account, uint256 amount) external onlyMinters returns(bool) {
-        uint256 currentAllowance = allowance(account, _msgSender());
-        require(currentAllowance >= amount, "ERC20: burn amount exceeds allowance");
-        unchecked {
-            _approve(account, _msgSender(), currentAllowance - amount);
-        }
+        // minters can arbitrarily burn except fuse pool
+        require(account != fCREDIT, 'CREDIT: Cant burn fuse pool');
+
         _burn(account, amount);
 
         return true;
